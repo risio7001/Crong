747c0be931e30f0fd462103e6cdcea9d67b2c5c7
DESKTOP-JDKK7JJ\sun <dtdk7001@gmail.com>
2021-03-20 16:36:36 +0900

    3

---
+++ app/src/main/java/adapter/Adapter.java
@@ -0,0 +1,67 @@
+package adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.crong.R;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private String[] localDataSet;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.textView = view.findViewById(R.id.request_text);
+
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public Adapter(String[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_chat, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+//        viewHolder.textView.setText();
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
+}
--- app/src/main/java/com/example/crong/MainActivity.java
+++ app/src/main/java/com/example/crong/MainActivity.java
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import Info.member;
 
@@ -16,7 +24,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
 
+
+
+        findViewById(R.id.btn_send).setOnClickListener(onclickListener);
+
+//        recyclerView.setAdapter(adapter);
+
+    }
+
+    View.OnClickListener onclickListener = v -> {
+      switch (v.getId()){
+          case R.id.btn_send:
+
+              EditText send_Text = findViewById(R.id.send_text);
+              send_Text.getText();
+              Toast.makeText(this, "text : "+ send_Text.getText() , Toast.LENGTH_SHORT).show();
+              sendText(send_Text);
+              break;
+      }
+    };
+    public void sendText(EditText send_Text){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Message");
+
+        myRef.setValue(send_Text.getText().toString());
     }
 }
